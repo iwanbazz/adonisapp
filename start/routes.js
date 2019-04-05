ae1583ddd80d566128078c70bdb93e945c64d11d
iwanbazz <iwan.bazz@gmail.com>
2019-04-05 16:02:42 +0700

    Added logging out method

--- start/routes.js
+++ start/routes.js
@@ -21,3 +21,7 @@ Route.get('/', 'JobController.home');
 Route.on('/signup').render('auth.signup');
 Route.post('/signup', 'UserController.create').validator('CreateUser');
 Route.on('/login').render('auth.login');
+Route.get('/logout', async ({ auth, response }) => {
+  await auth.logout();
+  return response.redirect('/');
+});

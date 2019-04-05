4c46da5caf722c62510012e18f2f48c5b81fa95b
iwanbazz <iwan.bazz@gmail.com>
2019-04-05 15:24:12 +0700

    Remove temp POST job

--- app/Controllers/Http/JobController.js
+++ app/Controllers/Http/JobController.js
@@ -6,14 +6,6 @@ const Job = use('App/Models/Job')
 class JobController {
     async home({view}) {
 
-        //create a job
-        const job = new Job;
-        job.title = 'My job title';
-        job.link = 'http://google.com';
-        job.description = 'My job description';
-
-        await job.save();
-
         // Fetch a job
         const jobs = await Job.all();
 

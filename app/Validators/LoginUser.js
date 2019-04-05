d8fa1d29269a9e852517406389fac1f12de98cf9
iwanbazz <iwan.bazz@gmail.com>
2019-04-05 16:21:08 +0700

    Create login component

---
+++ app/Validators/LoginUser.js
@@ -0,0 +1,25 @@
+'use strict'
+
+class CreateUser {
+  get rules () {
+    return {
+      'email': 'required|email',
+      'password': 'required'
+    }
+  }
+
+  get messages() {
+    return {
+      'required': 'woy. {{ field }} ini nih kudu di isi.',
+    }
+  }
+
+  async fails(error) {
+    this.ctx.session.withErrors(error)
+      .flashAll();
+    
+    return this.ctx.response.redirect('back');
+  }
+}
+
+module.exports = CreateUser
